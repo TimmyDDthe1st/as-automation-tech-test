1bb98ed36b1033f16e7ddefaaef5c9dae6c51a06
Tim Douglas <me@timdouglas.net>
2020-03-22 12:58:45 +0000

    finished the task

--- cypress/integration/solutions-page.spec.js
+++ cypress/integration/solutions-page.spec.js
@@ -5,11 +5,27 @@ const solutionspageSelectors = {
     headerText: ".et_pb_row_1 > .et_pb_column > .et_pb_module > .et_pb_text_inner > h2",
     subsectionNavBar:{
         navBar: "#menu-solutions",
-        earlyCareers: "#menu-solutions > #menu-item-3045 > a",
-        volumeRecruitment: "#menu-solutions > #menu-item-3044 > a",
-        experiencedHires: "#menu-solutions > #menu-item-3043 > a",
-        employeeDevelopment: "#menu-solutions > #menu-item-3042 > a",
-        customisationBranding: "#menu-solutions > #menu-item-3040 > a"
+        earlyCareers: {
+            link: "#menu-solutions > #menu-item-3045 > a",
+            firstParagraph: ".et_pb_column_2 > .et_pb_module > .et_pb_text_inner > :nth-child(2)"
+        },
+        volumeRecruitment: {
+            link: "#menu-solutions > #menu-item-3044 > a",
+            firstParagraph: ".et_pb_column_2 > .et_pb_module > .et_pb_text_inner > p",
+            seeOurOptionsHereLink: ".et_pb_text_inner > :nth-child(3) > a"
+        },
+        experiencedHires: {
+            link: "#menu-solutions > #menu-item-3043 > a",
+            firstParagraph: ".et_pb_column_2 > .et_pb_module > .et_pb_text_inner > p"
+        },
+        employeeDevelopment: {
+            link: "#menu-solutions > #menu-item-3042 > a",
+            firstParagraph: ".et_pb_column_2 > .et_pb_module > .et_pb_text_inner > :nth-child(1)"
+        },
+        customisationBranding: {
+            link: "#menu-solutions > #menu-item-3040 > a",
+            firstParagraph: ".et_pb_column_2 > .et_pb_module > .et_pb_text_inner"
+        } 
     }
 };
 
@@ -26,25 +42,56 @@ describe('Solutionspage', () => {
         cy.get(solutionspageSelectors.subsectionNavBar.navBar).should('be.visible');
     })
 
-    it('should contain "Early Careers"', () => {
-        const expected = "Early Careers";
-        cy.contains(solutionspageSelectors.subsectionNavBar.earlyCareers, expected);
+    it('should contain "Early Careers" and link through to the correct page', () => {
+        const expected = solutionsPageData.earlyCareers;
+        const firstParagraph = solutionsPageData.earlyCareersFirstParagraph;
+
+        cy.contains(solutionspageSelectors.subsectionNavBar.earlyCareers.link, expected).click();
+        cy.contains(solutionspageSelectors.headerText, expected);
+        cy.contains(solutionspageSelectors.firstParagraph, firstParagraph);
     })
 
-    it('should contain "Volume Recruitment"', () => {
-        const expected = "Volume Recruitment";
-        cy.contains(solutionspageSelectors.subsectionNavBar.volumeRecruitment, expected);
+    it('should contain "Volume Recruitment" and link through to the correct page', () => {
+        const expected = solutionsPageData.volumeRecruitment;
+        const firstParagraph = solutionsPageData.volumeRecruitementFirstParagraph;
+
+        cy.contains(solutionspageSelectors.subsectionNavBar.volumeRecruitment.link, expected).click();
+        cy.contains(solutionspageSelectors.headerText, expected);
+        cy.contains(solutionspageSelectors.subsectionNavBar.volumeRecruitment.firstParagraph, firstParagraph);
     })
-    it('should contain "Experienced Hires"', () => {
-        const expected = "Experienced Hires";
-        cy.contains(solutionspageSelectors.subsectionNavBar.experiencedHires, expected);
+
+    it('should take the user to the "Customisation / Branding" section when clicking "See our options here"', () => {
+        const expected = solutionsPageData.volumeRecruitment;
+        const expectedHeader = solutionsPageData.customisationBrandingHeader;
+
+        cy.contains(solutionspageSelectors.subsectionNavBar.volumeRecruitment.link, expected).click();
+        cy.contains(solutionspageSelectors.headerText, expected);
+        cy.get(solutionspageSelectors.subsectionNavBar.volumeRecruitment.seeOurOptionsHereLink).click();
+        cy.contains(solutionspageSelectors.headerText, expectedHeader);
     })
-    it('should contain "Employee Development"', () => {
-        const expected = "Employee Development";
-        cy.contains(solutionspageSelectors.subsectionNavBar.employeeDevelopment, expected);
+    it('should contain "Experienced Hires" and link through to the correct page', () => {
+        const expected = solutionsPageData.experiencedHires;
+        const firstParagraph = solutionsPageData.experiencedHiresFirstParagraph;
+
+        cy.contains(solutionspageSelectors.subsectionNavBar.experiencedHires.link, expected).click();
+        cy.contains(solutionspageSelectors.headerText, expected);
+        cy.contains(solutionspageSelectors.subsectionNavBar.experiencedHires.firstParagraph, firstParagraph);
     })
-    it('should contain "Customisation / Branding"', () => {
-        const expected = "Customisation / Branding";
-        cy.contains(solutionspageSelectors.subsectionNavBar.customisationBranding, expected);
+    it('should contain "Employee Development" and link through to the correct page', () => {
+        const expected = solutionsPageData.employeeDevelopment;
+        const firstParagraph = solutionsPageData.employeeDevelopmentFirstParagraph;
+        
+        cy.contains(solutionspageSelectors.subsectionNavBar.employeeDevelopment.link, expected).click();
+        cy.contains(solutionspageSelectors.headerText, expected);
+        cy.contains(solutionspageSelectors.subsectionNavBar.employeeDevelopment.firstParagraph, firstParagraph);
+    })
+    it('should contain "Customisation / Branding" and link through to the correct page', () => {
+        const expected = solutionsPageData.customisationBrandingLink;
+        const expectedHeader = solutionsPageData.customisationBrandingHeader
+        const firstParagraph = solutionsPageData.customisationBrandingFirstParagraph;
+
+        cy.contains(solutionspageSelectors.subsectionNavBar.customisationBranding.link, expected).click();
+        cy.contains(solutionspageSelectors.headerText, expectedHeader);
+        cy.contains(solutionspageSelectors.subsectionNavBar.customisationBranding.firstParagraph, firstParagraph);
     })
 });
